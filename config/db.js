d8ddec20d6e83832f7cacde66762dd7b47bb34db
MuhammadZainDev <109549362+MuhammadZainDev@users.noreply.github.com>
2025-02-09 17:10:22 +0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -9,10 +9,6 @@ const pool = new Pool({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     database: process.env.DB_NAME,
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false,
-    }
 });
 
 // Test the connection
